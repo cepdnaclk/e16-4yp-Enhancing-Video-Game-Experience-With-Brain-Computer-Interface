4ffa9a38f11f5b169f6a3850b3f9dfb1a34a767c
isu92neth <123csissa@gmail.com>
2022-08-16 17:40:46 +0530

    Add relax interval plots saving implementation

--- code/analysis thinking out loud/STFT.py
+++ code/analysis thinking out loud/STFT.py
@@ -11,20 +11,15 @@ import numpy as np
 
 
 def convertor(x: np.ndarray, fs: float, path:str):
-    x=x*1000000000
     f, t, zxx = signal.stft(x, fs, nperseg=256)
     plt.pcolormesh(t, f, np.abs(zxx), vmax=1000, shading='gouraud')
     plt.title('STFT')
     plt.ylabel('Frequency [Hz]')
     plt.xlabel('Time [sec]')
-
     print("Saving figure", path)
     plt.savefig(path, format="png")
-
-    
-    
+   
     #sys.exit()
-
 
 class STFT(Handler):
     def __init__(self):
@@ -33,14 +28,13 @@ class STFT(Handler):
     def handle(self, request: [str, np.ndarray]):
         # frequency
         convertor(request[1], 256, request[0])
-        
 
         if self.nextHandler is None:
             return
 
 
 if __name__ == "__main__":
-    file = Access_file(file_name='sub-01_ses-02')
+    file = Access_file(file_name='sub-03_ses-02')
     tags = Event_tag()
     chop = Chop()
     transformer = Transformer()
