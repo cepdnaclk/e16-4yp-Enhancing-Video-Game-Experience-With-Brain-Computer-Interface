b602877dfc4b99554dda16cef638d8422aed9ed0
isu92neth <123csissa@gmail.com>
2022-08-16 15:41:31 +0530

    Add figure saving for plotted data

--- code/analysis thinking out loud/STFT.py
+++ code/analysis thinking out loud/STFT.py
@@ -1,7 +1,5 @@
 import sys
-
 import matplotlib.pyplot as plt
-
 from CoR import Handler
 from scipy import signal
 import mne
@@ -12,16 +10,20 @@ from transformer import Transformer
 import numpy as np
 
 
-def convertor(x: np.ndarray, fs: float, name: str):
+def convertor(x: np.ndarray, fs: float, path:str):
+    x=x*1000000000
     f, t, zxx = signal.stft(x, fs, nperseg=256)
     plt.pcolormesh(t, f, np.abs(zxx), vmax=1000, shading='gouraud')
     plt.title('STFT')
     plt.ylabel('Frequency [Hz]')
     plt.xlabel('Time [sec]')
-    plt.show()
 
-    # todo remove this ans add file save
-    sys.exit()
+    print("Saving figure", path)
+    plt.savefig(path, format="png")
+
+    
+    
+    #sys.exit()
 
 
 class STFT(Handler):
@@ -31,13 +33,14 @@ class STFT(Handler):
     def handle(self, request: [str, np.ndarray]):
         # frequency
         convertor(request[1], 256, request[0])
+        
 
         if self.nextHandler is None:
             return
 
 
 if __name__ == "__main__":
-    file = Access_file(file_name='sub-03_ses-02')
+    file = Access_file(file_name='sub-01_ses-02')
     tags = Event_tag()
     chop = Chop()
     transformer = Transformer()
